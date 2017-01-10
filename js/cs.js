d2376000587d8bb0912f5a5aec0907356b1996b6
nakeshialorde <lordenakeshia@gmail.com>
2017-01-09 20:06:23 -0400

    updated websites

--- js/cs.js
+++
@@ -1,9 +0,0 @@
-new WOW().init();
-
-
-  	/* AUTHOR LINK */
-     $('.about-me-img').hover(function(){
-            $('.authorWindowWrapper').stop().fadeIn('fast').find('p').addClass('trans');
-        }, function(){
-            $('.authorWindowWrapper').stop().fadeOut('fast').find('p').removeClass('trans');
-        });
